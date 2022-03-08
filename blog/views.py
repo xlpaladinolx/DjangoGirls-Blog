22ed95f1d11feb83d70d1efb7ccacdcc1c6709aa
Paladino <pauloricardo.drds@gmail.com>
2022-03-08 14:36:21 -0300

    Atualizando site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def post_list(request):
+
+    return render(request, './blog/post_list.html', {})
+    #now = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
--- website/settings.py
+++ website/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
