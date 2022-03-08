431aed0fe7fecaf1d8baf098d74c46d6d601ef9b
Paladino <pauloricardo.drds@gmail.com>
2022-03-08 14:51:21 -0300

    New HTML2

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ import datetime
 
 def post_list(request):
 
-    return render(request, '/blog/post_list.html', {})
+    return render(request, './blog/post_list.html', {})
     #now = datetime.datetime.now()
     #html = "<html><body>It is now %s.</body></html>" % now
     #return HttpResponse(html)
